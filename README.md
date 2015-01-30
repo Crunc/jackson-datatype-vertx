c6e490c85912cfffba7136cd1316df990f6ec2a3
Hauke Jaeger <hauke.jaeger@nterra.com>
2015-01-30 16:14:41 +0100

    some documentation and test for <VertxJsonModule>

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # jackson-datatype-vertx
 Jackson2 Module which allows reading/writing org.vertx.java.core.json.JsonObject
+
+## Usage
+
+    ObjectMapper om = new ObjectMapper();
+    om.registerModule(new VertxJsonModule());
+    
+    // ...
+    
+    JsonObject object = om.readValue("{}", JsonObject.class);
+
+    // ...
+
+    String jsonObject = om.writeValueAsString(new JsonObject());
+
+    // ...
+
+    JsonArray object = om.readValue("[]", JsonArray.class);
+
+    // ...
+
+    String jsonArray = om.writeValueAsString(new JsonArray());
---
+++ src/test/java/de/crunc/jackson/datatype/vertx/VertxJsonModuleTest.java
@@ -0,0 +1,63 @@
+package de.crunc.jackson.datatype.vertx;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+
+import java.io.IOException;
+
+import static de.crunc.hamcrest.json.JsonMatchers.isJsonArray;
+import static de.crunc.hamcrest.json.JsonMatchers.isJsonObject;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Unit test for {@link VertxJsonModule}
+ * 
+ * @author Hauke Jaeger, hauke.jaeger@googlemail.com  
+ */
+public class VertxJsonModuleTest {
+
+    @Test
+    public void shouldAddJsonObjectSerializer() throws JsonProcessingException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new VertxJsonModule());
+
+        String json = om.writeValueAsString(JsonObjectBuilder.object()
+                .build());
+
+        assertThat(json, isJsonObject());
+    }
+
+    @Test
+    public void shouldAddJsonObjectDeserializer() throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new VertxJsonModule());
+
+        JsonObject object = om.readValue("{}", JsonObject.class);
+
+        assertThat(object, isJsonObject());
+    }
+
+    @Test
+    public void shouldAddJsonArraySerializer() throws JsonProcessingException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new VertxJsonModule());
+
+        String json = om.writeValueAsString(JsonArrayBuilder.array()
+                .build());
+
+        assertThat(json, isJsonArray());
+    }
+
+    @Test
+    public void shouldAddJsonArrayDeserializer() throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new VertxJsonModule());
+
+        JsonArray object = om.readValue("[]", JsonArray.class);
+
+        assertThat(object, isJsonArray());
+    }
+}
